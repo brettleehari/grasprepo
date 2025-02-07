21008232d58e3ff6a3b7dc96737aae4e8c323cec
hari <brettleehari@users.noreply.github.com>
2025-02-07 19:11:21 +0000

    Add churn metrics, add ranks provided a given file.

--- repomap.py
+++ repomap.py
@@ -51,7 +51,7 @@ class RepoMap:
         try:
             language = get_language(lang)
             parser = get_parser(lang)
-            print(f"Parsing file: {fname} with language: {lang}")
+            #print(f"Parsing file: {fname} with language: {lang}")
 
         except Exception as err:
             print(f"Warning: Skipping file {fname}: {err}", file=sys.stderr)
@@ -155,7 +155,7 @@ class RepoMap:
 
 
             tags = list(self.get_tags(fname, rel_fname))
-            print(f"Tags for file: {rel_fname} are: {tags}")
+            #print(f"Tags for file: {rel_fname} are: {tags}")
             #sys.exit(0) #breakpoint-2
 
             if tags is None:
@@ -171,9 +171,9 @@ class RepoMap:
                     references[tag.name].append(rel_fname)
 
         ##
-        dump(defines)
-        dump(references)
-        dump(personalization)
+        #dump(defines)
+        #dump(references)
+        #dump(personalization)
         if not references:
             references = dict((k, list(v)) for k, v in defines.items())
 
@@ -262,27 +262,48 @@ class RepoMap:
             net.add_edge(edge[0], edge[1])
 
         graphname = "graph"+str(time.time())+".html"
+        #Enable the below line to save the graph as an HTML file
         #net.show(graphname)
         for node, neighbors in G.adjacency():
-            print(f"Node {node}: {list(neighbors)}")
+            pass
+            #print(f"Node {node}: {list(neighbors)}")
         for (fname, ident), rank in ranked_definitions:
             # print(f"{rank:.03f} {fname} {ident}")
             #if fname in chat_rel_fnames:
             #    continue
             ranked_tags += list(definitions.get((fname, ident), []))
-        print("##################################")
-        print(ranked_tags)
+        print("############ranked tags######################")
+        #print(ranked_tags)
 
         fnames_already_included = set(rt[0] for rt in ranked_tags)
 
         top_rank = sorted([(rank, node) for (node, rank) in ranked.items()], reverse=True)
-        print(top_rank)
+        print("############top rank######################")
+        #print(top_rank)
+
+        # Check if the key exists in the ranked dictionary
+        file_path = 'request/tests/server.js'
+        if file_path in ranked:
+            # Find rank of a file
+            print(f" The rank of the file: {file_path} is {ranked[file_path]}")
+            # Find relative rank of the file in the list
+            #print the rank of the file in the list among the total number of files in the list
+            relative_rank = (top_rank.index((ranked[file_path], file_path)))
+            Total_files = len(top_rank)
+            print(f"Relative rank of the file in the list: {relative_rank} out of {Total_files}")
+            #what are the top 3 ranked files
+            top_3_files = top_rank[:3]
+            #print the top 3 ranked files and its rank
+            print(f"Top 3 ranked files in the repo: {top_3_files}")
+        else:
+            print(f"KeyError: '{file_path}' not found in ranked dictionary")
+
         #sys.exit(0) #breakpoint-3
         for rank, fname in top_rank:
             if fname not in fnames_already_included:
                 ranked_tags.append((fname,))
-        print("############ranked tags######################")
-        print(ranked_tags)
+        print("############ranked tags below######################")
+        #print(ranked_tags)
         #sys.exit(0) #breakpoint-4
         print("############ranked tags above######################")
         return ranked_tags
@@ -290,7 +311,7 @@ class RepoMap:
 
 
     def render_tree(self, abs_fname, rel_fname, lois):
-        print(f"Rendering tree for file: {rel_fname} with lines of interest: {lois}")
+        #print(f"Rendering tree for file: {rel_fname} with lines of interest: {lois}")
 
         # Read the file content directly
         try:
@@ -327,7 +348,7 @@ class RepoMap:
 
     def to_tree(self, tags):
 
-        print(f"Converting tags to tree structure, number of tags: {len(tags)}")
+        #print(f"Converting tags to tree structure, number of tags: {len(tags)}")
 
 
         cur_fname = None
@@ -371,8 +392,9 @@ class RepoMap:
 def get_scm_fname(lang):
     # Load the tags queries
     queries_dir = Path(__file__).parent / "queries"
-    print(f"Getting tags query file for language: {lang}")
-    print(f"Queries dir: {queries_dir}")    
+    
+    #print(f"Getting tags query file for language: {lang}")
+    #print(f"Queries dir: {queries_dir}")    
     scm_file = queries_dir / f"tree-sitter-{lang}-tags.scm"
     if scm_file.exists():
         return scm_file
@@ -389,7 +411,7 @@ def parse_dir(directory):
     for root, dirs, files in os.walk(directory):
         for file in files:
             src_files.append(os.path.join(root, file))
-    print(f"Found {len(src_files)} source files in {directory}")
+    #print(f"Found {len(src_files)} source files in {directory}")
     return src_files
 
 
@@ -403,7 +425,7 @@ def main():
     rank_tags = rm.get_ranked_tags(unpacked)
     #import pdb; pdb.set_trace()
     repo_tree = rm.to_tree(rank_tags)
-    print(repo_tree)
+    #print(repo_tree)
 
 if __name__ == "__main__":
     main()
